6213901b761b67c49af7775a4174986b6192916a
Rano-9 <esamatti.karvonen@pispalanit.fi>
2025-03-26 05:36:43 +0200

    viikko2

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   
   gem 'sqlite3'
+  gem 'pry-byebug'
 
 end
 
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,19 @@
 class Brewery < ApplicationRecord
-  has_many :beers
+  
+  include RatingAverage
+
+  has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
+
+  def print_report
+    puts self.name
+    puts "established at year #{self.year}"
+    puts "number of beers #{self.beers.count}"
+  end
+
+  def restart
+    self.year = 2022
+    puts "changed year to #{self.year}"
+  end
+
 end
